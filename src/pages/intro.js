ab829120d89251be5435d86d6284b48c769444b4
asaxena2019 <anushkasaxena2019@gmail.com>
2021-02-12 17:26:16 -0500

    added CV

--- src/pages/intro.js
+++ src/pages/intro.js
@@ -2,7 +2,7 @@ import React from "react"
 import anushka from "../images/anushkasaxena.png"
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
-import resume from "./resume.pdf"
+import CV from "./CV.pdf"
 
 import "./pages.css"
 
@@ -13,8 +13,8 @@ const Intro = () => (
     <h1>hi! i'm <ReactRotatingText items={['anushka', 'an engineer', 'the coffee coder']} /></h1>
     <Row>
       <Col>
-        <p><span role="img" aria-label="rocket">🚀</span> Welcome to my little corner of the web! I am a sophomore studying <a href="https://www.ece.cmu.edu/" target="_blank" rel="noreferrer">ECE @ CMU</a>. My goals in life including building human-centric tools, studying tech + society, and becoming a coffee connoisseur. Talk to me about products, policies, and startups!</p>
-        <p><span role="img" aria-label="star">🌟</span> Feel free to look at my <a href={resume} target="_blank" rel="noreferrer">resume</a> or contact me on any of my links. I'm also always down for a coffee chat!</p>
+        <p><span role="img" aria-label="hand">🚀</span> Welcome to my little corner of the web! I am a sophomore studying <a href="https://www.ece.cmu.edu/" target="_blank" rel="noreferrer">ECE @ CMU</a>. My goals in life including building human-centric tools, studying tech + society, and becoming a coffee connoisseur. Talk to me about products, policies, and startups!</p>
+        <p><span role="img" aria-label="star">🌟</span> Feel free to look at my <a href={CV} target="_blank" rel="noreferrer">CV</a> or contact me on any of my links. I'm also always down for a coffee chat!</p>
       </Col>
       <Col className="col-section">
         <img src={anushka} alt="profile"/>
